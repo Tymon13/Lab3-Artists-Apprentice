e5966be4020729c8765bfefd745abbedeae29614
Kamil Tymiński <kamil_tym@wp.pl>
2021-04-24 20:31:32 +0200

    Add camera preview in the main fragment
    Ah sh*t, here we go again.

--- app/src/main/java/pl/edu/pwr/lab3/i238162/ui/main/MainFragment.java
+++ app/src/main/java/pl/edu/pwr/lab3/i238162/ui/main/MainFragment.java
@@ -1,34 +1,90 @@
 package pl.edu.pwr.lab3.i238162.ui.main;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
 
 import pl.edu.pwr.lab3.i238162.R;
 
 public class MainFragment extends Fragment {
+    private static final int CAMERA_PERMISSION_CODE = 13;
 
+    private Activity parentActivity;
     private MainViewModel mainViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        mainViewModel.getText()
-                     .observe(getViewLifecycleOwner(), new Observer<String>() {
-                                  @Override
-                                  public void onChanged(@Nullable String s) {
-                                      textView.setText(s);
-                                  }
-                              });
+        parentActivity = requireActivity();
+
+        if (ContextCompat.checkSelfPermission(parentActivity, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+            startPreview();
+        } else {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }
+
         return root;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startPreview();
+            } else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(parentActivity);
+                builder.setMessage(R.string.camera_permission_needed_dialog)
+                       .setPositiveButton(R.string.button_ok, null)
+                       .create()
+                       .show();
+            }
+        }
+    }
+
+    private void startPreview() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(
+                parentActivity);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(parentActivity));
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+
+        PreviewView cameraPreviewView = parentActivity.findViewById(R.id.camera_preview_view);
+        preview.setSurfaceProvider(cameraPreviewView.getSurfaceProvider());
+
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                                                                    .build();
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+    }
+
 }
